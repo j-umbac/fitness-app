fda6a40177ded2c33064032522c216f8985048db
Joshua Umbac <joshuarheyumbac14@gmail.com>
2021-12-14 21:50:58 +0800

    Changed catch phrase

---
+++ precache-manifest.786f314af69c6891407ea50f3f94674c.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311a0835aef847fd1b86",
+    "url": "/fitness-app/css/admin.6a396e59.css"
+  },
+  {
+    "revision": "5a4bcb79479fb2ffea44",
+    "url": "/fitness-app/css/app.4e4ce61e.css"
+  },
+  {
+    "revision": "806809d3b3831300aaf1",
+    "url": "/fitness-app/css/start.ff6635fa.css"
+  },
+  {
+    "revision": "b6d514fb25e188c34d738949d79581e6",
+    "url": "/fitness-app/data.json"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/fitness-app/fonts/lato-v16-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/fitness-app/fonts/lato-v16-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/fitness-app/fonts/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/fitness-app/fonts/lato-v16-latin-regular.b8ee546a.woff"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/fitness-app/fonts/montserrat-v14-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/fitness-app/fonts/montserrat-v14-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/fitness-app/fonts/montserrat-v14-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/fitness-app/fonts/montserrat-v14-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "26d42c9428780e545a540bbb50c84bce",
+    "url": "/fitness-app/fonts/montserrat-v14-latin-900.26d42c94.woff"
+  },
+  {
+    "revision": "58cd789700850375b834e8b6776002eb",
+    "url": "/fitness-app/fonts/montserrat-v14-latin-900.58cd7897.woff2"
+  },
+  {
+    "revision": "3df2c011bc1447742a11e469346da105",
+    "url": "/fitness-app/img/exercises/1.jpg"
+  },
+  {
+    "revision": "c04450bf730746c4211449b37590ba10",
+    "url": "/fitness-app/img/exercises/13.jpg"
+  },
+  {
+    "revision": "07967f1598c063bec50d0aeb68cbfb51",
+    "url": "/fitness-app/img/exercises/14.jpg"
+  },
+  {
+    "revision": "07967f1598c063bec50d0aeb68cbfb51",
+    "url": "/fitness-app/img/exercises/15.jpg"
+  },
+  {
+    "revision": "09f6b6f6a8e8722d568af78325103787",
+    "url": "/fitness-app/img/exercises/2.jpg"
+  },
+  {
+    "revision": "e7843242fba095e4cc842f0669411f52",
+    "url": "/fitness-app/img/exercises/3.jpg"
+  },
+  {
+    "revision": "b07f833de24fe29d74d95f90ae33a88b",
+    "url": "/fitness-app/img/exercises/4.jpg"
+  },
+  {
+    "revision": "62a25fa5b1f67ee1814ad0102959a1f3",
+    "url": "/fitness-app/img/exercises/5.jpg"
+  },
+  {
+    "revision": "45348c31723978b2116c57c96ed0e323",
+    "url": "/fitness-app/img/exercises/6.jpg"
+  },
+  {
+    "revision": "6e478fbf32a58d4d3e9d35d49cd1459f",
+    "url": "/fitness-app/img/exercises/7.jpg"
+  },
+  {
+    "revision": "9b579019c67d97ff7c4505d9da419759",
+    "url": "/fitness-app/img/exercises/9.jpg"
+  },
+  {
+    "revision": "e62d67c0294d87075a338d415e9d5ff9",
+    "url": "/fitness-app/img/favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "bef52cf8ae112043726f2e680d6d5cae",
+    "url": "/fitness-app/img/favicons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "17babcaa897f09175eb85868c4795524",
+    "url": "/fitness-app/img/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "6fe04602bf677d14c4799353af33e5cf",
+    "url": "/fitness-app/img/favicons/browserconfig.xml"
+  },
+  {
+    "revision": "063be3e988db32da31c1deb4ecf2f52b",
+    "url": "/fitness-app/img/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "2e109903ecd966e282059bed98efbbb3",
+    "url": "/fitness-app/img/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "8d0fd06b9dd9f72869c1ff8b66a7dbdb",
+    "url": "/fitness-app/img/favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "8883b3a167ea90cb5357ee0a2ce3b2c7",
+    "url": "/fitness-app/img/favicons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "f3257d6bac0d2f59ca84f14514ab85f4",
+    "url": "/fitness-app/img/logo.f3257d6b.svg"
+  },
+  {
+    "revision": "b57ac394abab42b90363dd43782a62a2",
+    "url": "/fitness-app/index.html"
+  },
+  {
+    "revision": "311a0835aef847fd1b86",
+    "url": "/fitness-app/js/admin.44f1c200.js"
+  },
+  {
+    "revision": "5a4bcb79479fb2ffea44",
+    "url": "/fitness-app/js/app.84e03b5d.js"
+  },
+  {
+    "revision": "99939719185a694269ff",
+    "url": "/fitness-app/js/chunk-vendors.692ee10c.js"
+  },
+  {
+    "revision": "56c7ffdef2e5299269d5",
+    "url": "/fitness-app/js/exercises.12ca45c0.js"
+  },
+  {
+    "revision": "806809d3b3831300aaf1",
+    "url": "/fitness-app/js/start.59757cfb.js"
+  },
+  {
+    "revision": "d307293ffa87ae4d93ac",
+    "url": "/fitness-app/js/workouts.25b3f206.js"
+  },
+  {
+    "revision": "a9fedc2bd2935691ef79c2ab4a750af8",
+    "url": "/fitness-app/manifest.json"
+  }
+]);
